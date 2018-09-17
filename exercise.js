ecd01f26af5c2774ef54e9d0edc3ca8548996b4c
Valerie Osei <valerie.osei@gmail.com>
2018-09-16 22:28:27 -0400

    practiced accessing info from nested objects in array

---
+++ exercise.js
@@ -0,0 +1,49 @@
+var project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  dueDate : "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     dueDate: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     dueDate: "January 1, 2018"
+    },
+    {description: "compile results",
+     dueDate: "November 10, 2018"
+    },
+    {description: "version 1",
+     dueDate: "January 15, 2019"
+    },
+    {description: "revisions",
+     dueDate: "March 30, 2019"
+    },
+    {description: "version 2",
+     dueDate: "July 12, 2019"
+    },
+    {description: "final edit",
+     dueDate: "October 1, 2019"
+    },
+    {description: "final version",
+     dueDate: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     dueDate: "December 1, 2019"
+    }
+  ]
+}
+
+// console.log(project.committee)
+//
+// console.log(names)
+// console.log(projects)
+
+  project.steps.forEach(function(step, index){
+    step.committeeMember = project.committee[index % project.committee.length];
+
+  });
+
+// console.log(project.committee.push("Val"))
+console.log(project)
+console.log(project.committee.length)
